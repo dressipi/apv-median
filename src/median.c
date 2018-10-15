c2d1fc611abc3c56f1b4707898bc11c69fa7c393
J.J. Green <jim.green@dressipi.com>
2018-10-15 10:40:58 +0100

    stub for the median module

--- src/median.c
+++ src/median.c
@@ -3,4 +3,63 @@
   Copyright (c) Dressipi 2018
 */
 
+#include <string.h>
+
 #include "median.h"
+#include "avltree.h"
+
+struct bin_t
+{
+  size_t count;
+  double max;
+};
+
+struct histogram_t
+{
+  size_t n;
+  avltree_t *tree;
+};
+
+
+static int bin_cmp(const void *b1, const void *b2)
+{
+  double
+    max1 = ((bin_t*)b1)->max,
+    max2 = ((bin_t*)b2)->max;
+
+  if (max1 < max2) return -1;
+  if (max1 > max2) return 1;
+  return 0;
+}
+
+static void* bin_dup(void *b)
+{
+  bin_t *bdup;
+
+  if ((bdup = malloc(sizeof(bin_t))) == NULL)
+    return NULL;
+
+  return memcpy(bdup, b, sizeof(bin_t));
+}
+
+extern histogram_t* new_histogram(size_t n)
+{
+  histogram_t *hist;
+
+  if ((hist = malloc(sizeof(histogram_t))) != NULL)
+    {
+      avltree_t *tree;
+
+      if ((tree = avlnew(bin_cmp, bin_dup, free)) != NULL)
+	{
+	  hist->n = n;
+	  hist->tree = tree;
+
+	  return hist;
+	}
+
+      free(hist);
+    }
+
+  return NULL;
+}
--- src/median.h
+++ src/median.h
@@ -6,4 +6,11 @@
 #ifndef MEDIAN_H
 #define MEDIAN_H
 
+#include <stdlib.h>
+
+typedef struct bin_t bin_t;
+typedef struct histogram_t histogram_t;
+
+extern histogram_t* new_histogram(size_t);
+
 #endif
