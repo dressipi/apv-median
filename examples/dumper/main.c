f969076adc288be326fcf5615892da5a188db938
J.J. Green <jim.green@dressipi.com>
2018-10-22 14:52:11 +0100

    initial version of histogram dumper completed

--- examples/dumper/main.c
+++ examples/dumper/main.c
@@ -2,7 +2,11 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+#include <histogram.h>
+
 #include "options.h"
+
+#define PATH_LEN 128
 
 int main(int argc, char** argv)
 {
@@ -12,6 +16,37 @@ int main(int argc, char** argv)
     {
       fprintf(stderr,"failed to parse command line\n");
       return EXIT_FAILURE;
+    }
+
+  histogram_t *hist;
+
+  if ((hist = histogram_new(info.bins_arg)) == NULL)
+    {
+      fprintf(stderr,"failed to initialise histogram\n");
+      return EXIT_FAILURE;
+    }
+
+  size_t count = 0;
+  double value;
+  char path[PATH_LEN];
+
+  while (scanf("%lf", &value) != EOF)
+    {
+      if (histogram_add(hist, value) != 0)
+	{
+	  fprintf(stderr, "error adding %f to histogram\n", value);
+	  return EXIT_FAILURE;
+	}
+
+      snprintf(path, PATH_LEN, "%s/%08zi.json", info.directory_arg, count);
+
+      if (histogram_json_save(hist, path) != 0)
+	{
+	  fprintf(stderr, "error saving histogram to %s\n", path);
+	  return EXIT_FAILURE;
+	}
+
+      count++;
     }
 
   return EXIT_SUCCESS;
