c059dc4f1d125f554dfa81d90ad515dde3572f59
J.J. Green <jim.green@dressipi.com>
2019-10-31 14:56:56 +0000

    initial percentile implementation

--- src/median.c
+++ src/median.c
@@ -7,43 +7,9 @@
 #include <math.h>
 
 #include "median.h"
+#include "percentile.h"
 
 extern int median(const histogram_t *hist, double *value)
 {
-  size_t k = histogram_num_bins(hist);
-  bin_t *bins = histogram_bins(hist);
-  double total = 0.0;
-
-  for (size_t i = 0 ; i < k ; i++)
-    total += bins[i].count;
-
-  double
-    partial = 0.0,
-    min = 0.0;
-
-  for (size_t i = 0 ; i < k ; i++)
-    {
-      double
-	max = bins[i].max,
-	count = bins[i].count,
-	x = min + ((total / 2) - partial) * (max - min) / count;
-
-      if (x < max)
-	{
-	  *value = x;
-	  return 0;
-	}
-
-      partial += count;
-      min = max;
-    }
-
-  /*
-    We should never reach here, since the median of any
-    non-empty set of numbers should be between the min
-    and max values (possibly equal to, in the degenerate
-    case of equal values).
-   */
-
-  return 1;
+  return percentile(hist, 50.0, value);
 }
---
+++ src/percentile.c
@@ -0,0 +1,52 @@
+/*
+  percentile.c
+  Copyright (c) Stylemania Ltd. 2018, 2019
+*/
+
+#include <string.h>
+#include <math.h>
+
+#include "percentile.h"
+
+extern int percentile(const histogram_t *hist, double percent, double *value)
+{
+  if ((percent < 0) || (percent > 100))
+    return 1;
+
+  size_t k = histogram_num_bins(hist);
+  bin_t *bins = histogram_bins(hist);
+  double total = 0.0;
+
+  for (size_t i = 0 ; i < k ; i++)
+    total += bins[i].count;
+
+  double
+    partial = 0.0,
+    min = 0.0;
+
+  for (size_t i = 0 ; i < k ; i++)
+    {
+      double
+	max = bins[i].max,
+	count = bins[i].count,
+	x = min + ((percent / 100.0) - partial) * (max - min) / count;
+
+      if (x < max)
+	{
+	  *value = x;
+	  return 0;
+	}
+
+      partial += count;
+      min = max;
+    }
+
+  /*
+    We should never reach here, since a percentile of any
+    non-empty set of numbers should be between the min
+    and max values (possibly equal to, in the degenerate
+    case of equal values).
+   */
+
+  return 1;
+}
